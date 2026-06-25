23a4b98b7eab71fad1c58596500bb550c84c350c
Xelvanas <xelvanas@gmail.com>
2026-06-25 17:57:26 +1000

    Extract navigation provider boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { NavContext } from "./features/navigation/nav-context";
-import { useNavigationController } from "./features/navigation/use-navigation-controller";
+import { NavigationProvider } from "./features/navigation/NavigationProvider";
 import { Shell } from "./features/shell/Shell";
 
 export default function App() {
-  const nav = useNavigationController();
-
   return (
-    <NavContext.Provider value={nav}>
-      <Shell nav={nav} />
-    </NavContext.Provider>
+    <NavigationProvider>{(nav) => <Shell nav={nav} />}</NavigationProvider>
   );
 }
