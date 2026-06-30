b31fe6e71258364945b0107f5b89786da40b4c3d
Xelvanas <xelvanas@gmail.com>
2026-07-01 03:28:12 +1000

    Split new thread popover blocks

---
+++ src/features/shell/shoal/components/NewThreadActions.tsx
@@ -0,0 +1,20 @@
+interface NewThreadActionsProps {
+  submitDisabled: boolean;
+  onClose: () => void;
+}
+
+export function NewThreadActions({
+  submitDisabled,
+  onClose,
+}: NewThreadActionsProps) {
+  return (
+    <div className="new-thread-actions">
+      <button type="button" onClick={onClose}>
+        Cancel
+      </button>
+      <button type="submit" disabled={submitDisabled}>
+        Create
+      </button>
+    </div>
+  );
+}
