9490e8ccd1bc35bb236c8725a60c41612b993947
Xelvanas <xelvanas@gmail.com>
2026-06-30 20:05:56 +1000

    Extract Shoal presentational components

---
+++ src/features/shell/shoal/components/ShoalIcons.tsx
@@ -0,0 +1,19 @@
+export function FolderIcon() {
+  return (
+    <svg viewBox="0 0 18 18" aria-hidden="true">
+      <path d="M2.5 5.5h5l1.3 1.7h6.7v6.2a1.1 1.1 0 0 1-1.1 1.1H3.6a1.1 1.1 0 0 1-1.1-1.1z" />
+      <path d="M2.5 5.5V4.6a1.1 1.1 0 0 1 1.1-1.1h3.1l1.2 2" />
+    </svg>
+  );
+}
+
+export function RoleplayCardIcon() {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+      <path d="M5 5.5h5.7c1.2 0 2.3.5 3.1 1.3v11.7c-.8-.8-1.9-1.3-3.1-1.3H5z" />
+      <path d="M13.8 6.8c.8-.8 1.9-1.3 3.1-1.3H19v11.7h-2.1c-1.2 0-2.3.5-3.1 1.3" />
+      <path d="M7.6 9h3.2" />
+      <path d="M16.2 9h1.1" />
+    </svg>
+  );
+}
